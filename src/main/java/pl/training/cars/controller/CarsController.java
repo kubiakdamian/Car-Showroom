6327b4978d3e463174f1aebd7d948c9c1ecb2fb2
Damian Kubiak <damian.kubiak@cybercom.com>
2018-07-09 11:46:29 +0200

    Getting cars by horsePower added

--- src/main/java/pl/training/cars/controller/CarsController.java
+++ src/main/java/pl/training/cars/controller/CarsController.java
@@ -1,9 +1,12 @@
 package pl.training.cars.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import pl.training.cars.model.Car;
@@ -20,5 +23,18 @@ public class CarsController {
     	cars = new Cars();
         return cars.getCars();
     }
-
+    
+    @GetMapping("/cars/{horsePower}")
+    public List<Car> getCarsByHorsePower(@PathVariable(value = "horsePower") int horsePower){
+    	cars = new Cars();
+    	List<Car> result = new ArrayList<>();
+    	
+    	for(Car car : cars.getCars()) {
+    		if(car.getHorsePower() >= horsePower) {
+    			result.add(car);
+    		}
+    	}
+    	
+        return result;
+    }
 }
