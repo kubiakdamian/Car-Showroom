db1876d691cad3604d533cb433cd19db8b1f0b67
Damian Kubiak <damian.kubiak@cybercom.com>
2018-07-10 15:59:02 +0200

    Swagger working

--- src/main/java/pl/training/cars/config/SwaggerConfig.java
+++ src/main/java/pl/training/cars/config/SwaggerConfig.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Configuration;
 
 import com.google.common.base.Predicates;
 
+
 import lombok.Setter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -29,9 +29,7 @@ public class SwaggerConfig {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())
 				.select()
-				.apis(RequestHandlerSelectors.any())
 				.paths(Predicates.not(PathSelectors.regex("/error")))
-				.paths(Predicates.not(PathSelectors.regex("/Swagger")))
 				.build();
 	}
 	
